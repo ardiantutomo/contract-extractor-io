a1f5573baee5696b99a788096ee097cdc08423b7
Ardiant Utomo <ardiantutomo@gmail.com>
2020-07-09 12:16:03 +0700

    Updates

---
+++ precache-manifest.6c634fdd61c82c891002aa5818efded9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "146f1a46c2248134dd0b5279f7cefe8c",
+    "url": "/contract-extractor-io/index.html"
+  },
+  {
+    "revision": "94c430133251dc6949c3",
+    "url": "/contract-extractor-io/static/css/2.9edc93b8.chunk.css"
+  },
+  {
+    "revision": "7ca47393e5b47957463e",
+    "url": "/contract-extractor-io/static/css/main.3fecd08e.chunk.css"
+  },
+  {
+    "revision": "94c430133251dc6949c3",
+    "url": "/contract-extractor-io/static/js/2.35d8233b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contract-extractor-io/static/js/2.35d8233b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ca47393e5b47957463e",
+    "url": "/contract-extractor-io/static/js/main.4115c1ba.chunk.js"
+  },
+  {
+    "revision": "6b269bb348bb600b74c0",
+    "url": "/contract-extractor-io/static/js/runtime-main.e322a8ec.js"
+  }
+]);
