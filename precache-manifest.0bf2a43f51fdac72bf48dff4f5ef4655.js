91cf9cb7d528a6c3b6fd6867e70aa7f2e5d807b5
Ardiant Utomo <ardiantutomo@gmail.com>
2020-07-13 16:41:14 +0700

    Updates

--- precache-manifest.0bf2a43f51fdac72bf48dff4f5ef4655.js
+++ precache-manifest.0bf2a43f51fdac72bf48dff4f5ef4655.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "35b1e7b02304d8fb87539f2b95c84c36",
+    "revision": "52bb73df195f6a224a5c7460a53b0171",
     "url": "/contract-extractor-io/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/contract-extractor-io/static/css/2.9edc93b8.chunk.css"
   },
   {
-    "revision": "cbc7406fda4340d68c8a",
+    "revision": "f40d71de0e814fdeb042",
     "url": "/contract-extractor-io/static/css/main.27a7ee61.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/contract-extractor-io/static/js/2.72a20f6a.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "cbc7406fda4340d68c8a",
-    "url": "/contract-extractor-io/static/js/main.cfff9dd0.chunk.js"
+    "revision": "f40d71de0e814fdeb042",
+    "url": "/contract-extractor-io/static/js/main.eabb0206.chunk.js"
   },
   {
     "revision": "6b269bb348bb600b74c0",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/contract-extractor-io/precache-manifest.5581b5ea99dd560717bfbcb8cd5ead4e.js"
+  "/contract-extractor-io/precache-manifest.0bf2a43f51fdac72bf48dff4f5ef4655.js"
 );
 
 self.addEventListener('message', (event) => {
